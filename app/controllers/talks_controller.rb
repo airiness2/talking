2aa6fdce14ef4179cd4654a9c75da5529fed92da
reiko watanabe <blackgothy@gmail.com>
2019-05-18 10:18:33 +0000

    アクション設定、バリデーション設定、ロケール設定

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,2 +1,62 @@
 class TalksController < ApplicationController
+  before_action :set_talk, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @talks = Talk.all
+  end
+  
+  def new
+    if params[:back]
+      @talk = Talk.new(talk_params)
+    else
+      @talk = Talk.new
+    end
+  end
+  
+  def create
+    @talk = Talk.new(talk_params)
+    if @talk.save
+      redirect_to talks_path, notice: "つぶやきを作成しました。"
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @talk = Talk.find(params[:id])
+  end
+  
+  def edit
+    @talk = Talk.find(params[:id])
+  end
+  
+  def update
+    @talk = Talk.find(params[:id])
+    if @talk.update(talk_params)
+      redirect_to talks_path, notice: "つぶやきを編集しました。"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @talk.destroy
+    redirect_to talks_path, notice: "つぶやきを削除しました。"
+  end
+  
+  def confirm
+    @talk = Talk.new(talk_params)
+    render :new if @talk.invalid?
+  end
+  
+  private
+  
+  def talk_params
+    params.require(:talk).permit(:content)
+  end
+  
+  def set_talk
+    @talk = Talk.find(params[:id])
+  end
+
 end
--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,2 +1,3 @@
 class Talk < ApplicationRecord
+   validates :content,  presence: true, length: { maximum: 140 } 
 end
